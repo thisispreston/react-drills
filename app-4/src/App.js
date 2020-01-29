ce3305fe42db54d4a4a8a9b3e4c1744f8ffff520
Preston Jenkins <prestonjenkins@Prestons-MacBook-Pro.local>
2020-01-29 16:23:21 -0700

    App4 Done

--- app-4/src/App.js
+++ app-4/src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      username: '',
+      password: '',
+    }
+  }
+
+  handleChange1 = (val) => {
+    this.setState({username: val})
+  }
+
+  handleChange2 = (val) => {
+    this.setState({password: val})
+  }
+
+  alertUser = () => {
+    alert(`Username: ${this.state.username}  Password: ${this.state.password}`)
+  }
+
+  render () {
+    return (
+      <div className='App'>
+        <input onChange={e => this.handleChange1(e.target.value)}/>
+        <input onChange={e => this.handleChange2(e.target.value)}/>
+        <button onClick={this.alertUser}>Login</button>
+      </div>
+    )
+  }
+
 }
 
 export default App;
